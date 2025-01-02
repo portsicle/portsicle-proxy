fa835ed275a767c48bbf9c72582c2f44210596e9
Amit Suthar <sutharamit707@gmail.com>
2025-01-02 23:45:34 +0530

    feat: block ad banners

--- internal/database/db.go
+++ internal/database/db.go
@@ -10,6 +10,85 @@ import (
 
 const dbFile = "blocked_domains.db"
 
+func getAdDomains() []string {
+	return []string{
+		"aax.amazon-adsystem.com",
+		"ade.googlesyndication.com",
+		"adnxs.com",
+		"ads.stickyadstv.com",
+		"analytics.google.com",
+		"ad-delivery.net",
+		"awin1.com",
+		"ads.com",
+		"adroll.com",
+		"amplitude.nav.no",
+		"appnexus.com",
+		"banners.affiliatefuture.com",
+		"bidswitch.net",
+		"bttrack.com",
+		"bingads.microsoft.com",
+		"criteo.net",
+		"crazyegg.com",
+		"collect.mopinion.com",
+		"cdn1.vntsm.com",
+		"clickbank.net",
+		"commissionjunction.com",
+		"doubleclick.net",
+		"disqus.com",
+		"deploy.mopinion.com",
+		"dynalyst-sync.adtdp.com",
+		"f.vimeocdn.com",
+		"googleads.g.doubleclick.net",
+		"googleadservices.com",
+		"google-analytics.com",
+		"googlesyndication.com",
+		"go1.aniview.com",
+		"hotjar.com",
+		"livefyre.com",
+		"mixpanel.com",
+		"mgid.com",
+		"media.adfrontiers.com",
+		"openx.net",
+		"outbrain.com",
+		"optimizely.com",
+		"pubmatic.com",
+		"pagead1.googlesyndication.com",
+		"perfectaudience.com",
+		"pagead2.googlesyndication.com",
+		"rubiconproject.com",
+		"revcontent.com",
+		"retargeter.com",
+		"refersion.com",
+		"s.amazon-adsystem.com",
+		"static.a-ads.com",
+		"shb.richaudience.com",
+		"segment.io",
+		"steelhousemedia.com",
+		"snowplowanalytics.com",
+		"shareasale.com",
+		"spot.im",
+		"stats.bannersnack.com",
+		"s2s.aniview.com",
+		"tpe.googlesyndication.com",
+		"track.wargaming-aff.com",
+		"track1.aniview.com",
+		"track4.aniview.com",
+		"taboola.com",
+		"tracker.cbx-rtb.com",
+		"www.googletagmanager.com",
+		"www.adsurve.com",
+	}
+}
+
+/*
+I guess we can toggle css styles to remove the ad placeholder as well?
+
+common classes and selectors I found from inspecting various sites:
+adsbygoogle, adsbygoogle-noablate, data-adsbygoogle-status="done, script[src*='adnxs.com'], etc
+
+PS: I Still can't figure out how to block 'World of Tank' Ads they suck!!
+*/
+
 func InitDB() (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
@@ -66,6 +145,11 @@ func GetBlockedDomains(db *sql.DB) (map[string]struct{}, error) {
 		if err := rows.Scan(&domain); err != nil {
 			return nil, fmt.Errorf("failed to fetch a blocked domain: %v", err)
 		}
+		domains[domain] = struct{}{}
+	}
+
+	// simply appending the ad domains along with blocked domains map
+	for _, domain := range getAdDomains() {
 		domains[domain] = struct{}{}
 	}
 
